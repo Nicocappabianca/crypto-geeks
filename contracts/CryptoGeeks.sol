0f00f5920dde0beef9e077d262946bbfcc04765d
nicocappabianca <nicolas.cappabianca@gmail.com>
2022-05-16 22:13:36 -0300

    [WIP] - CryptoGeeks contract: tokenURI function

--- contracts/CryptoGeeks.sol
+++ contracts/CryptoGeeks.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "./Base64.sol"; 
 
 
 contract CryptoGeeks is ERC721, ERC721Enumerable { 
@@ -23,6 +24,30 @@ contract CryptoGeeks is ERC721, ERC721Enumerable {
     // current: is the ID with which you mint
     _safeMint(msg.sender, current);
     _idCounter.increment(); 
+  }
+
+  function tokenURI(uint256 tokenId) 
+    public 
+    view  
+    override  
+    returns (string memory) 
+  { 
+    require(
+      _exists(tokenId), 
+      "ERC721 Metadata: URI query for non-existent token"
+    ); 
+
+    string memory jsonURI = Base64.encode(
+      abi.encodePacked(
+          '{ "name": "CryptoGeek #',
+          tokenId,
+          '", "description": "CyrptoGeeks are randomized Avatars stored on chain", "image": "',
+          "// TODO: Calculate image URL",
+          '"}'
+      )
+    );
+
+    return string(abi.encodePacked("data:application/json;base64,", jsonURI));
   }
 
   // Overrides required by Solidity
